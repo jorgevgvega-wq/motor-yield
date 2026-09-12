c0cccefb77d96d3ef28b2ca16cfec22e362bdaf9
jorgevgvega-wq <jorgevgvega@gmail.com>
2026-09-11 22:35:12 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jv-field-tools-v5-2-rfo-pro';
+const CACHE='jv-field-tools-v5-3-rfo-pro';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
